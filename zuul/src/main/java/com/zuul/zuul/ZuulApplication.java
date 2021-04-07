087367d3977a852d512a258babd53fab185e0a6e
unknown <akulaglic2@etf.unsa.ba>
2021-04-07 16:58:20 +0200

    zuul service

---
+++ zuul/src/main/java/com/zuul/zuul/ZuulApplication.java
@@ -0,0 +1,26 @@
+package com.zuul.zuul;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+	@Bean
+	public SimpleFilter simpleFilter() {
+	  return new SimpleFilter();
+	}
+
+}
