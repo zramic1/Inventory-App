4b72a289cbfb933eb1dbd35499ab11880cd8ce6a
dpozderac1 <37264041+dpozderac1@users.noreply.github.com>
2021-05-05 17:25:41 +0200

    Layout is added with sidebar and submenus

--- inventory-ui/src/index.js
+++ inventory-ui/src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Meni from "./components/Meni.js"
+import Zaglavlje from "./components/Zaglavlje.js"
+
+import "antd/dist/antd.css";
+import { Layout } from "antd";
+
+
+const { Header, Sider, Content } = Layout;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+    <Layout style={{ minHeight: "100vh" }}>
+      <Header style={{ minHeight: "80px" }}>
+        <Zaglavlje />
+      </Header>
+      <Layout>
+        <Sider><Meni /></Sider>
+        <Content>Content</Content>
+      </Layout>
+    </Layout>
+
+
+
   </React.StrictMode>,
   document.getElementById('root')
 );
